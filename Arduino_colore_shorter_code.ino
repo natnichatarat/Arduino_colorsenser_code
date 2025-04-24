3de5375fa4a148d01d2cbb4ff09ea25907675b95
Natnicha Tarat <118197278+natnichatarat@users.noreply.github.com>
2025-04-24 22:34:18 +0700

    Update Arduino_colore_shorter_code.ino

--- Arduino_colore_shorter_code.ino
+++ Arduino_colore_shorter_code.ino
@@ -1,16 +1,131 @@
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+#define BLYNK_TEMPLATE_ID "TMPL6mVvSl9dx"
+#define BLYNK_TEMPLATE_NAME "Quickstart Template"
+#define BLYNK_AUTH_TOKEN "bctMa1gXeG-ON535XvBDsi6V_7FkGbN7"
+#define BLYNK_PRINT Serial
+
 #include <Servo.h>
+#include <BlynkSimpleEsp8266.h>
 
-#define S0 2
-#define S1 3
-#define S2 4
-#define S3 5
-#define sensorOut 6
+#define S0 D6
+#define S1 D7
+#define S2 D3
+#define S3 D4
+#define sensorOut D5
 
-Servo topServo;
+int red = 0;  
+int green = 0;  
+int blue = 0;
+int frequency = 0;
+int color = 0;
+int pos = 0; // Initialize pos here
+char auth[] = "bctMa1gXeG-ON535XvBDsi6V_7FkGbN7"; // Token directly here
+const char* ssid = "iPhone opal";
+const char* password = "88888888";
+
+const char* http_username = "admin";
+const char* http_password = "admin";
+
+Servo myServo;
 Servo bottomServo;
 
-int frequency = 0;
-int color=0;
+const char* PARAM_INPUT_1 = "state";
+
+const int output = 2;
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>ESP Web Server</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    html {font-family: Arial; display: inline-block; text-align: center;}
+    h2 {font-size: 2.6rem;}
+    body {max-width: 600px; margin:0px auto; padding-bottom: 10px;}
+    .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
+    .switch input {display: none}
+    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 34px}
+    .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 68px}
+    input:checked+.slider {background-color: #2196F3}
+    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
+  </style>
+</head>
+<body>
+  <h2>ESP Web Server</h2>
+  <button onclick="logoutButton()">Logout</button>
+  <p>Ouput - GPIO 2 - State <span id="state">%STATE%</span></p>
+  %BUTTONPLACEHOLDER%
+<script>function toggleCheckbox(element) {
+  var xhr = new XMLHttpRequest();
+  if(element.checked){ 
+    xhr.open("GET", "/update?state=1", true); 
+    document.getElementById("state").innerHTML = "ON";  
+  }
+  else { 
+    xhr.open("GET", "/update?state=0", true); 
+    document.getElementById("state").innerHTML = "OFF";      
+  }
+  xhr.send();
+}
+function logoutButton() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/logout", true);
+  xhr.send();
+  setTimeout(function(){ window.open("/logged-out","_self"); }, 1000);
+}
+</script>
+</body>
+</html>
+)rawliteral";
+
+const char logout_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <p>Logged out or <a href="/">return to homepage</a>.</p>
+  <p><strong>Note:</strong> close all web browser tabs to complete the logout process.</p>
+</body>
+</html>
+)rawliteral";
+
+// Replaces placeholder with button section in your web page
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "BUTTONPLACEHOLDER"){
+    String buttons ="";
+    String outputStateValue = outputState();
+    buttons+= "<p><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"output\" " + outputStateValue + "><span class=\"slider\"></span></label></p>";
+    return buttons;
+  }
+  if (var == "STATE"){
+    if(digitalRead(output)){
+      return "ON";
+    }
+    else {
+      return "OFF";
+    }
+  }
+  return String();
+}
+
+String outputState(){
+  if(digitalRead(output)){
+    return "checked";
+  }
+  else {
+    return "";
+  }
+  return "";
+}
+
 
 void setup() {
   pinMode(S0, OUTPUT);
@@ -18,121 +133,128 @@ void setup() {
   pinMode(S2, OUTPUT);
   pinMode(S3, OUTPUT);
   pinMode(sensorOut, INPUT);
-
-  // Setting frequency-scaling to 20%
   digitalWrite(S0, HIGH);
   digitalWrite(S1, LOW);
 
-  topServo.attach(7);
-  bottomServo.attach(8);
+  myServo.attach(D2); // Attach servo to pin D2
+  bottomServo.attach(D1);
+  
+  Serial.begin(115200);
+  Blynk.begin(auth, ssid, password);
+  pinMode(output, OUTPUT);
+  digitalWrite(output, LOW);
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
 
-  Serial.begin(9600);
+  // Print ESP Local IP Address
+  Serial.println(WiFi.localIP());
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    if(!request->authenticate(http_username, http_password))
+      return request->requestAuthentication();
+    request->send_P(200, "text/html", index_html, processor);
+  });
+    
+  server.on("/logout", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(401);
+  });
+
+  server.on("/logged-out", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", logout_html, processor);
+  });
+
+  // Send a GET request to <ESP_IP>/update?state=<inputMessage>
+  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    if(!request->authenticate(http_username, http_password))
+      return request->requestAuthentication();
+    String inputMessage;
+    String inputParam;
+    // GET input1 value on <ESP_IP>/update?state=<inputMessage>
+    if (request->hasParam(PARAM_INPUT_1)) {
+      inputMessage = request->getParam(PARAM_INPUT_1)->value();
+      inputParam = PARAM_INPUT_1;
+      digitalWrite(output, inputMessage.toInt());
+    }
+    else {
+      inputMessage = "No message sent";
+      inputParam = "none";
+    }
+    Serial.println(inputMessage);
+    request->send(200, "text/plain", "OK");
+  });
+  
+  // Start server
+  server.begin();
 }
 
 void loop() {
+  Blynk.run();
 
-  topServo.write(115);
-  delay(500);
-  
-   topServo.write(140);
-  delay(500);
-  topServo.write(65);
-  delay(5000);
-  
-  color = readColor();
-  delay(10);  
-  Serial.print("\ncolor = ");Serial.println(color); 
-  switch (color) {
-    
-
-    case 2:
-    bottomServo.write(50);
-    delay(500);
-    break;
-
-    case 3:
-    bottomServo.write(75);
-    delay(500);
-    break;
-
-    //case 4:
-    //bottomServo.write(100);
-    //break;
-
-    case 5:
-    bottomServo.write(125);
-    delay(500);
-    break;
-
-    
-    
-    case 0:
-    bottomServo.write(90);
-    break;
-  }
-  //delay(300);
-  
-
-  topServo.write(25);
-  delay(500);
-  color=0;
-}
-
-// Custom Function - readColor()
-int readColor() {
-  // Setting red filtered photodiodes to be read
+  // Read red
   digitalWrite(S2, LOW);
   digitalWrite(S3, LOW);
-  // Reading the output frequency
-  frequency = pulseIn(sensorOut, LOW);
-  int R = frequency;
-  // Printing the value on the serial monitor
-  Serial.print("R= ");//printing name
-  Serial.print(frequency);//printing RED color frequency
-  Serial.print("  ");
-  delay(50);
+  red = pulseIn(sensorOut, LOW);
 
-  // Setting Green filtered photodiodes to be read
+  // Read green
   digitalWrite(S2, HIGH);
   digitalWrite(S3, HIGH);
-  // Reading the output frequency
-  frequency = pulseIn(sensorOut, LOW);
-  int G = frequency;
-  // Printing the value on the serial monitor
-  Serial.print("G= ");//printing name
-  Serial.print(frequency);//printing RED color frequency
-  Serial.print("  ");
-  delay(50);
+  green = pulseIn(sensorOut, LOW);
 
-  // Setting Blue filtered photodiodes to be read
+  // Read blue
   digitalWrite(S2, LOW);
   digitalWrite(S3, HIGH);
-  // Reading the output frequency
-  frequency = pulseIn(sensorOut, LOW);
-  int B = frequency;
-  // Printing the value on the serial monitor
-  Serial.print("B= ");//printing name
-  Serial.print(frequency);//printing RED color frequency
-  Serial.println("  ");
-  delay(50);
+  blue = pulseIn(sensorOut, LOW);
+  
+  // Map the color
+  if (blue < 55 && blue > 45) {
+    color = 1; // Blue
+    bottomServo.write(20);
+    delay(500);
+  } else if (green < 30 && green > 14) {
+    color = 2; // Green
+    bottomServo.write(90);
+    delay(500);
+  } else if (red < 41 && red > 30) {
+    color = 3; // Red
+    bottomServo.write(180);
+    delay(500);
+  }
 
-  //if(R<45 & R>32 & G<65 & G>55){
-    //color = 1; // Red
-  //}
-  if(R<88 & R>59 ){
-    color = 2; // Orange
-  }
-  if( G<25 & G>10){
-    color = 3; // Green
-  }
-  //if(R<38 & R>24 & G<44 & G>30){
-    //color = 4; // Yellow
-  //}
-  if(B<61 &B>55){
-    color = 5; // Brown
-  }
-  //if (G<58 & G>45 & B<40 &B>26){
-    //color = 6; // Blue
-  //}
-  return color;  
+  // Send color value to Blynk
+  Blynk.virtualWrite(V1, color);  // Assuming V1 is configured in your Blynk app
+
+  // Print color values for debugging
+  Serial.print("R=");
+  Serial.print(red);
+  Serial.print(" G=");
+  Serial.print(green);
+  Serial.print(" B=");
+  Serial.println(blue);
+
+  delay(100); // Delay for stability
+  
+  // Check the state of the Button widget
+  Blynk.syncVirtual(V3); // Read the state of the Button widget
 }
+
+BLYNK_WRITE(V3) 
+{
+  int val = param.asInt(); 
+  if (val == 0) {
+    myServo.write(0);
+  } else if (val == 1) {
+    for (pos = 0; pos <= 180; pos += 1) { // Change int pos to pos, as it's already declared
+      myServo.write(pos);
+      delay(15); // Adjust the delay as needed for your servo speed
+    }
+    delay(5000); // Added missing semicolon
+  }
+}
+
+
